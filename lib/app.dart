79dead959fdb6b2466a24012191004945db18803
anh.le <anh.le@supremetech.vn>
2023-04-10 10:47:50 +0700

    test workflow

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,6 @@
-import 'package:convex_bottom_bar/convex_bottom_bar.dart';
 import 'package:firebase_dynamic_links/firebase_dynamic_links.dart';
 import 'package:flutter/material.dart';
+import 'package:go_route/route.dart';
 import 'package:go_route/screens/Profile.dart';
 import 'package:go_route/screens/category.dart';
 import 'package:go_route/screens/shopping_cart.dart';
@@ -28,10 +28,11 @@ class _AppState extends State<App> {
     final PendingDynamicLinkData? initialLink =
         await FirebaseDynamicLinks.instance.getInitialLink();
     if (initialLink != null) {
-      context.pushNamed('category', params: {'category': 'Watch'});
+      // context.pushNamed('category', params: {'category': 'Watch'});
+      deepLinkNavigate(context, initialLink.link.path);
     }
     FirebaseDynamicLinks.instance.onLink.listen((dynamicLinkData) {
-      context.pushNamed('category', params: {'category': 'Kitchen'});
+      deepLinkNavigate(context, dynamicLinkData.link.path);
     }).onError((error) {});
   }
 
@@ -40,24 +41,53 @@ class _AppState extends State<App> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Shopping App'),
-      ),
-      body: _listPage[selectedPage],
-      bottomNavigationBar: ConvexAppBar(
-        items: const [
-          TabItem(icon: Icons.home),
-          TabItem(
-            icon: Icons.shopping_cart,
-          ),
-          TabItem(
-            icon: Icons.people,
-          ),
+        actions: [
+          IconButton(
+              onPressed: () {
+                context.pushNamed('shoppingCard');
+              },
+              icon: const Icon(Icons.shopping_cart))
         ],
-        initialActiveIndex: selectedPage,
-        onTap: (int index) {
-          setState(() {
-            selectedPage = index;
-          });
-        },
+      ),
+      body: HomePage(),
+      // bottomNavigationBar: ConvexAppBar(
+      //   items: const [
+      //     TabItem(icon: Icons.home),
+      //     TabItem(
+      //       icon: Icons.shopping_cart,
+      //     ),
+      //     TabItem(
+      //       icon: Icons.people,
+      //     ),
+      //   ],
+      //   initialActiveIndex: selectedPage,
+      //   onTap: (int index) {
+      //     setState(() {
+      //       selectedPage = index;
+      //     });
+      //   },
+      // ),
+      drawer: Container(
+        color: Colors.white,
+        width: MediaQuery.of(context).size.width * 2 / 3,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(
+              height: 100,
+            ),
+            TextButton(
+                onPressed: () {
+                  context.pushNamed(ScreenPaths.profile);
+                  Navigator.of(context).pop();
+                },
+                child: const Text('Profile'))
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => deepLinkNavigate(context, ''),
+        child: const Icon(Icons.add),
       ),
     );
   }
